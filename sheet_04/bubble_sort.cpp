1660f8df73c823b7d8bdb6dcbaa658033f8cfbc8
malte <malte.fillmer@gmail.com>
2021-05-21 08:34:58 +0000

    add bubble sort implementation

--- sheet_04/bubble_sort.cpp
+++ sheet_04/bubble_sort.cpp
@@ -16,17 +16,47 @@
 #include <vector>
 
 // TODO provide an implementation
-void bubble_sort(std::vector<int> &v);
+void bubble_sort(std::vector<int> &v)
+{
+  for (int i = 0; i < v.size(); i++)
+  {
+    for (int j = i + 1; j < v.size(); j++)
+    {
+      if (v[i] > v[j])
+      {
+        int temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+      }
+    }
+  }
+}
 
 // TODO provide an implementation
-void bubble_sort(std::vector<int> &v, size_t from, size_t to);
+void bubble_sort(std::vector<int> &v, size_t from, size_t to)
+{
+  for (int i = from; i < to; i++)
+  {
+    for (int j = i + 1; j < to; j++)
+    {
+      if (v[i] > v[j])
+      {
+        int temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+      }
+    }
+  }
+}
 
-std::ostream &operator<<(std::ostream &os, const std::vector<int> &v) {
+std::ostream &operator<<(std::ostream &os, const std::vector<int> &v)
+{
   std::copy(v.begin(), v.end(), std::ostream_iterator<int>(os, " "));
   return os;
 }
 
-int main() {
+int main()
+{
   std::vector<int> v = {1, 5, 6, 23, 7, 8, 9, 21, 12, 4};
   std::cout << "unsorted: ";
   std::cout << v << '\n';
